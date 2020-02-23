5ec50372cab1e614f40fe71baf12f82731326342
Chris <jukes311@gmail.com>
2020-02-23 11:30:01 -0500

    Convert to constants

--- src/DatabaseTest.java
+++ src/DatabaseTest.java
@@ -6,17 +6,20 @@ import java.io.OutputStream;
 import java.util.Properties;
 
 public class DatabaseTest {
+	private static final String DB_PASSWORD = "dbPassword";
+	private static final String DB_USER = "dbUser";
+	private static final String DB_URL = "dbUrl";
 	private static final String DB_PROPERTIES = "db.properties";
 	private String dbUrl;
 	private String dbUser;
 	private String dbPassword;
-	
+	private Properties globalProps;
 	
 	public void writeProperties() throws IOException {
 		Properties prop = new Properties();
-		prop.setProperty("dbUrl", "localhost");
-		prop.setProperty("dbUser", "username");
-		prop.setProperty("dbPassword", "password");
+		prop.setProperty(DB_URL, "localhost");
+		prop.setProperty(DB_USER, "username");
+		prop.setProperty(DB_PASSWORD, "password");
 
 		try (OutputStream out = new FileOutputStream(DB_PROPERTIES)) {
 			prop.store(out, "Database Properties File");
@@ -29,9 +32,9 @@ public class DatabaseTest {
 		try (InputStream in = new FileInputStream(DB_PROPERTIES)) {
 			Properties prop = new Properties();
 			prop.load(in);
-			dbUrl = prop.getProperty("dbUrl");
-			dbUser = prop.getProperty("dbUser");
-			dbPassword = prop.getProperty("dbPassword");
+			dbUrl = prop.getProperty(DB_URL);
+			dbUser = prop.getProperty(DB_USER);
+			dbPassword = prop.getProperty(DB_PASSWORD);
 		}
 	}	
 	
