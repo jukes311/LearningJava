2ab31cae1e8662937a3aaaa8da2d02359038539e
Chris <jukes311@gmail.com>
2020-02-23 11:56:02 -0500

    Added JDBC Example using Apache Derby DB.

--- src/LearningJava.java
+++ src/LearningJava.java
@@ -1,9 +1,16 @@
+import java.io.IOException;
+import java.sql.SQLException;
 
 public class LearningJava {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+		DatabaseTest test = new DatabaseTest();
+		try {
+			test.testConnection();
+		} catch (IOException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
